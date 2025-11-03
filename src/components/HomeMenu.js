ad682bdefec67ba922fb6673974ce624199d960b
felubarreiro <fbarreiro@udesa.edu.ar>
2025-11-03 10:10:08 -0300

    creacion proyecto y home menu

---
+++ src/components/HomeMenu.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Entypo from '@expo/vector-icons/Entypo';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Home from "../screens/Home"
+import Profile from "../screens/Profile"
+const Tab = createBottomTabNavigator()
+
+
+class HomeMenu extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={Home} options={{tabBarIcon: ()=><Entypo name="home" size={24} color="black" />}}/>
+                <Tab.screen name="Create" component={Create} options={{tabBarIcon:()=> <AntDesign name="plus-square" size={24} color="black" />}}/>
+                <Tab.Screen name="Profile" component={Profile} options={{tabBarIcon: ()=> <MaterialIcons name="account-circle" size={24} color="black" />}}/>
+                
+            </Tab.Navigator>
+        )
+    }
+}
+
+export default HomeMenu
