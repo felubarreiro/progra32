114f6cd9d042954057425882a9fb1b87ca549be7
felubarreiro <fbarreiro@udesa.edu.ar>
2025-11-03 10:27:57 -0300

    login,register

---
+++ src/screens/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Component } from "react";
+import { StyleSheet, Text, View, Image, FlatList, Pressable } from 'react-native';
+
+class Home extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        
+
+    return(
+        <View>
+<Text>Home</Text>
+
+</View>
+    )
+}}
+
+export default Home
